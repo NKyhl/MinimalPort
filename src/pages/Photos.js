230b9ce4f10652d57323cde1e30be145b7050fe3
Nolan Kyhl <nolan@kyhl.com>
2024-12-24 15:25:38 -0600

    Photos

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,7 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import "./Photos.css";
+
+const importAll = (r) => r.keys().map(r);
+
 export default function Photos() {
+  const navigate = useNavigate();
+
+  const photos = importAll(require.context('../images/photography', false, /\.(png|jpe?g|svg)$/));
 
   return (
     <div className="App">
+      <div className="scrollable-page">
+        <header className="photo-header">
+          <h1 className="photo-title">Photos</h1>
+          <button 
+            className="photo-close-btn"
+            onClick={() => navigate('/')}
+            >×</button>
+        </header>
+        <div className="photo-grid">
+          {Object.values(photos).map((src, index) => (
+            <img key={index} src={src} alt={"Photo" + index} className="photo-grid-item"/>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
